a5a069339ac4f9b2311fea7ea03385cb54201b52
q4a <q4arus@ya.ru>
2020-06-30 01:13:51 +0300

    add PowerShell script for update VS toolset

---
+++ update_for_vs2019.ps1
@@ -0,0 +1,27 @@
+## run script to change PlatformToolset from v120 to v140
+
+$NewToolset = '<PlatformToolset>v142'
+$OldToolsets = @(
+"<PlatformToolset>v120_xp",
+"<PlatformToolset>v120"
+)
+
+$Paths = @(
+"proj",
+"sdks/bullet3/build3/vs2010",
+"sdks/freetype-2.10.0/builds/windows/vc2010/",
+"sdks/graphix/proj/",
+"sdks/graphix/sdks/common/proj/",
+"sdks/ogg/libogg/win32/VS2010/",
+"sdks/ogg/libvorbis/win32/VS2010/",
+"source/common/proj/"
+)
+
+For ($i=0; $i -lt $Paths.Length; $i++) {
+    ForEach ($File in (Get-ChildItem -Path $Paths[$i] -Filter *.vcxproj -Recurse | %{$_.FullName})) {
+		ForEach ($OldToolset in $OldToolsets) {
+			(Get-Content $File) -Replace $OldToolset,$NewToolset |
+				Set-Content $File
+		}
+	}
+}
